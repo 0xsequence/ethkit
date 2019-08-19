f3457094f78ff7b7bd8e5682aaa4fd605d933aa0
AJ Ostrow <alexander.ostrow@gmail.com>
2019-08-19 15:53:14 -0400

    remove allocation

--- ethgas/ema.go
+++ ethgas/ema.go
@@ -6,7 +6,7 @@ import (
 
 // NewEMA(decay) returns a new exponential moving average. It weighs new values more than
 // existing values according to the decay. For example: NewEMA(0.05) would give 5% weight
-// to new values and 95% weight to past values. Common to use 2/(selected time period+1).
+// to the present and 95% weight to the past. Common to use 2/(selected time period+1).
 func NewEMA(decay float64) *EMA {
 	return &EMA{decay: big.NewFloat(decay)}
 }
@@ -25,8 +25,7 @@ func (ema *EMA) Tick(price *big.Int) *big.Int {
 		new(big.Float).Sub(big.NewFloat(1), ema.decay),
 		new(big.Float).SetInt(ema.value),
 	)
-	next := new(big.Float).Add(current, past)
-	ema.value, _ = next.Int(nil)
+	new(big.Float).Add(current, past).Int(ema.value)
 	return ema.value
 }
 
