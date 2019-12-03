936d8a8b8c23851ac9b60a63ffbc400f3a384075
Peter Kieltyka <peter.kieltyka@gmail.com>
2019-12-03 10:22:40 -0500

    ethwallet: remove GetNextTxnNonce, leave up to the call to handle nonces

--- ethwallet/provider.go
+++ ethwallet/provider.go
@@ -3,34 +3,18 @@ package ethwallet
 import (
 	"context"
 	"math/big"
-	"sync"
-	"time"
 
 	"github.com/arcadeum/ethkit/ethrpc"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
 )
 
 // WalletProvider is a helper to query the provider in context of the wallet address
 type WalletProvider struct {
 	wallet   *Wallet
 	provider *ethrpc.Provider
-
-	lastBlockNum  *big.Int
-	lastBlockPoll time.Time
-
-	nonceIndex map[common.Address][2]*big.Int
-
-	mu sync.Mutex
 }
 
 func (w *WalletProvider) NewTransactor(ctx context.Context) (*bind.TransactOpts, error) {
-	// Get the next txn nonce for the wallet in a thread-safe way
-	nonce, err := w.GetNextTxnNonce(ctx)
-	if err != nil {
-		return nil, err
-	}
-
 	// Get suggested gas price, the user can change this on their own too
 	gasPrice, err := w.provider.SuggestGasPrice(ctx)
 	if err != nil {
@@ -38,10 +22,10 @@ func (w *WalletProvider) NewTransactor(ctx context.Context) (*bind.TransactOpts,
 	}
 
 	auth := w.wallet.Transactor()
-	auth.Nonce = nonce
 	auth.Value = big.NewInt(0)
 	auth.GasLimit = 0 // (0 = estimate)
 	auth.GasPrice = gasPrice
+	auth.Nonce = nil // remains unset, will be auto-set or user can specify
 
 	return auth, nil
 }
@@ -60,56 +44,6 @@ func (w *WalletProvider) GetTransactionCount(ctx context.Context) (uint64, error
 		return 0, err
 	}
 	return nonce, nil
-}
-
-// GetNextTxnNonce will return the next account txn nonce in a thread-safe way.
-func (w *WalletProvider) GetNextTxnNonce(ctx context.Context) (*big.Int, error) {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-
-	blockNum := w.lastBlockNum
-	if blockNum == nil || time.Since(w.lastBlockPoll) > (2*time.Second) {
-		block, err := w.provider.HeaderByNumber(ctx, nil)
-		if err != nil {
-			return nil, err
-		}
-		blockNum = block.Number
-		w.lastBlockNum = blockNum
-		w.lastBlockPoll = time.Now()
-	}
-
-	if w.nonceIndex == nil {
-		w.nonceIndex = map[common.Address][2]*big.Int{}
-	}
-
-	address := w.wallet.Address()
-	idx, ok := w.nonceIndex[address]
-	if !ok {
-		idx = [2]*big.Int{blockNum, nil}
-	}
-
-	if idx[1] == nil || blockNum.Uint64() > idx[0].Uint64() {
-		txnCount, err := w.GetTransactionCount(ctx)
-		if err != nil {
-			return nil, err
-		}
-
-		idx[0] = blockNum
-		if idx[1] == nil || txnCount > idx[1].Uint64() {
-			idx[1] = big.NewInt(int64(txnCount))
-		} else if txnCount == idx[1].Uint64() {
-			idx[1] = idx[1].Add(idx[1], big.NewInt(1))
-		} else {
-			// means txnCount is < idx[1] -- but now?
-			// lets do nothing in this case
-			// return nil, errors.Errorf("ethwallet: nonce index out-of-sync for address:%s txnCount:%d nonce:%d", address.String(), txnCount, idx[1].Uint64())
-		}
-	} else {
-		idx[1] = idx[1].Add(idx[1], big.NewInt(1))
-	}
-
-	w.nonceIndex[address] = idx
-	return idx[1], nil
 }
 
 // TODO
