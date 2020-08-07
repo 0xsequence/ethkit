6192955b9de0dfdf85d43cb7c5b05a7d9521d77b
Jonathan Cooper <cooper@cooperthecoder.com>
2020-08-07 08:24:52 -0500

    Added test for invalid wallet mnemonic and to ensure that wallets generated with a mnemonic return the same mnemonic to callers

--- ethwallet/hdnode_test.go
+++ ethwallet/hdnode_test.go
@@ -39,6 +39,7 @@ func TestHDNodeDerivationPath(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, hdnode)
 	assert.Equal(t, "0xe0C9828dee3411A28CcB4bb82a18d0aAd24489E0", hdnode.Address().Hex())
+	assert.Equal(t, testMnemonic, hdnode.Mnemonic())
 
 	err = hdnode.DeriveAccountIndex(1)
 	assert.NoError(t, err)
@@ -47,4 +48,23 @@ func TestHDNodeDerivationPath(t *testing.T) {
 	err = hdnode.DeriveAccountIndex(0)
 	assert.NoError(t, err)
 	assert.Equal(t, "0xe0C9828dee3411A28CcB4bb82a18d0aAd24489E0", hdnode.Address().Hex())
+}
+
+func TestHDNodeDerivationFailsWithInvalidMnemonic(t *testing.T) {
+	testInvalidMnemonics := []string{
+		// one word too short
+		"sentence roast truly flower surface power begin ocean silent debate funny",
+
+		// first word is jamaican patois, not valid bip39 word
+		"wahgwaan sentence roast truly flower surface power begin ocean silent debate funny",
+
+		// one word too long
+		"outdoor sentence roast truly flower surface power begin ocean silent debate funny funny",
+	}
+
+	for _, mnemonic := range testInvalidMnemonics {
+		hdnode, err := ethwallet.NewHDNodeFromMnemonic(mnemonic, nil)
+		assert.Errorf(t, err, "Expected error for invalid mnemonic %v", mnemonic)
+		assert.Nilf(t, hdnode, "Expected nil hdnode for invalid mnemonic %v", mnemonic)
+	}
 }
