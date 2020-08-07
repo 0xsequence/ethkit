7c1b80f2d9d7cf840ae078a6265b6a52a48fb718
Jonathan Cooper <cooper@cooperthecoder.com>
2020-08-07 08:23:30 -0500

    Moved privateKey field access after error handling to prevent nil panics if ECPrivKey returns error

--- ethwallet/hdnode.go
+++ ethwallet/hdnode.go
@@ -244,11 +244,11 @@ func derivePrivateKey(masterKey *hdkeychain.ExtendedKey, path accounts.Derivatio
 	}
 
 	privateKey, err := key.ECPrivKey()
-	privateKeyECDSA := privateKey.ToECDSA()
 	if err != nil {
 		return nil, err
 	}
 
+	privateKeyECDSA := privateKey.ToECDSA()
 	return privateKeyECDSA, nil
 }
 
