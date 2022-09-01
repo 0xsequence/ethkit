48de9d10dac988b97be72b93b029469027900240
William Hua <william@attente.ca>
2022-09-01 17:25:59 -0400

    ethgas: arbitrum actual gas price paid strategy

--- ethgas/compute.go
+++ ethgas/compute.go
@@ -142,6 +142,70 @@ func newGasGaugeArbitrum() *gasGaugeArbitrum {
 }
 
 func (c *gasGaugeArbitrum) ComputeSuggestedGasPrice(ctx context.Context, gasGauge *GasGauge, block *ethmonitor.Block) (*SuggestedGasPrice, error) {
-	panic("TODO")
-	return nil, nil
+	g := gasGauge
+
+	txns := block.Transactions()
+	if len(txns) == 0 {
+		return nil, nil
+	}
+
+	var gasPrices []*big.Int
+	for range txns {
+		gasPrices = append(gasPrices, block.BaseFee())
+	}
+
+	// Case if there are no gas prices sampled from any of the transactions, lets
+	// use the block's base fee once
+	if len(gasPrices) == 0 {
+		gasPrices = append(gasPrices, block.BaseFee())
+	}
+
+	// sort gas list from low to high
+	sort.Slice(gasPrices, func(i, j int) bool {
+		return gasPrices[i].Cmp(gasPrices[j]) < 0
+	})
+
+	// calculate gas price samples via histogram method
+	hist := gasPriceHistogram(gasPrices)
+	high, mid, low := hist.samplePrices()
+
+	if high.Sign() == 0 || mid.Sign() == 0 || low.Sign() == 0 {
+		return nil, nil
+	}
+
+	// get gas price from list at percentile position (old method)
+	// high = percentileValue(gasPrices, 0.9) / ONE_GWEI  // expensive
+	// mid = percentileValue(gasPrices, 0.75) / ONE_GWEI // mid
+	// low = percentileValue(gasPrices, 0.4) / ONE_GWEI  // low
+
+	// TODO: lets consider the block GasLimit, GasUsed, and multipler of the node
+	// so we can account for the utilization of a block on the network and consider it as a factor of the gas price
+
+	instant := max(high, g.minGasPrice)
+	fast := max(mid, g.minGasPrice)
+	standard := max(low, g.minGasPrice)
+	slow := max(new(big.Int).Div(new(big.Int).Mul(standard, big.NewInt(85)), big.NewInt(100)), g.minGasPrice)
+
+	// tick
+	c.emaSlow.Tick(slow)
+	c.emaStandard.Tick(standard)
+	c.emaFast.Tick(fast)
+	c.emaInstant.Tick(instant)
+
+	// compute final suggested gas price by averaging the samples
+	// over a period of time
+	sgp := SuggestedGasPrice{
+		BlockNum:    block.Number(),
+		BlockTime:   block.Time(),
+		InstantWei:  new(big.Int).Set(c.emaInstant.Value()),
+		FastWei:     new(big.Int).Set(c.emaFast.Value()),
+		StandardWei: new(big.Int).Set(c.emaStandard.Value()),
+		SlowWei:     new(big.Int).Set(c.emaSlow.Value()),
+		Instant:     new(big.Int).Div(c.emaInstant.Value(), ONE_GWEI_BIG).Uint64(),
+		Fast:        new(big.Int).Div(c.emaFast.Value(), ONE_GWEI_BIG).Uint64(),
+		Standard:    new(big.Int).Div(c.emaStandard.Value(), ONE_GWEI_BIG).Uint64(),
+		Slow:        new(big.Int).Div(c.emaSlow.Value(), ONE_GWEI_BIG).Uint64(),
+	}
+
+	return &sgp, nil
 }
