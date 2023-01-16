7bf62721366dcee41636eb324922bebe85191b97
Patryk Kalinowski <patryk@kalinowski.dev>
2023-01-16 22:06:12 +0300

    WIP ethrpc2 implementation

--- ethrpc2/ethrpc.go
+++ ethrpc2/ethrpc.go
@@ -1,9 +1,13 @@
 package ethrpc2
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
 	"math/big"
 	"net/http"
+	"sync/atomic"
 
 	"github.com/goware/breaker"
 	"github.com/goware/cachestore"
@@ -13,84 +17,96 @@ import (
 type Provider struct {
 	log        logger.Logger
 	nodeURL    string
-	httpClient *http.Client
+	httpClient httpClient
 	br         breaker.Breaker
 
 	chainID *big.Int
 	cache   cachestore.Store[[]byte]
+	lastID  atomic.Uint32
 }
 
-func NewProvider(nodeURL string, optHTTPClient ...*http.Client) (*Provider, error) {
-	return nil, nil
+func NewProvider(ctx context.Context, nodeURL string, options ...Option) (*Provider, error) {
+	p := &Provider{
+		nodeURL:    nodeURL,
+		httpClient: http.DefaultClient,
+	}
+
+	for _, opt := range options {
+		opt(p)
+	}
+
+	var err error
+	p.chainID, err = p.ChainID(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
 }
 
-func (s *Provider) ChainID(ctx context.Context) (*big.Int, error) {
-	return nil, nil
-}
+func (p *Provider) Do(ctx context.Context, calls ...Call) error {
+	if len(calls) == 0 {
+		return nil
+	}
 
-func (s *Provider) GetBalance(ctx context.Context) error {
+	batchReq := make([]*jsonrpcMessage, 0, len(calls))
+	for i, call := range calls {
+		if call.err != nil {
+			// TODO: store and return the error but execute the rest of the batch?
+			return fmt.Errorf("call %d has an error: %w", i, call.err)
+		}
+
+		jrpcReq := call.request
+		jrpcReq.ID = p.lastID.Add(1)
+		batchReq = append(batchReq, jrpcReq)
+	}
+
+	var reqBody any = batchReq
+	if len(batchReq) == 1 {
+		reqBody = batchReq[0]
+	}
+
+	b, err := json.Marshal(reqBody)
+	if err != nil {
+		return fmt.Errorf("failed to marshal JSONRPC request: %w", err)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, p.nodeURL, bytes.NewBuffer(b))
+	if err != nil {
+		return fmt.Errorf("failed to initialize http.Request: %w", err)
+	}
+	req = req.WithContext(ctx)
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := p.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send request: %w", err)
+	}
+	defer res.Body.Close()
+
+	var (
+		results []jsonrpcMessage
+		target  any = &results
+	)
+	if len(batchReq) == 1 {
+		results = make([]jsonrpcMessage, 1)
+		target = &results[0]
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(target); err != nil {
+		return fmt.Errorf("failed to unmarshal response: %w", err)
+	}
+
+	for i, result := range results {
+		// TODO: handle batch errors
+		if e := result.Error; e != nil {
+			return fmt.Errorf("JSONRPC error %d: %s", e.Code, e.Message)
+		}
+		if err := calls[i].resultFn(result.Result); err != nil {
+			return fmt.Errorf("failed to store result value: %w", err)
+		}
+	}
+
 	return nil
 }
 
-// TODO: .. each method..
-// eth_XXXX
-
-// TODO: lets add batch request support.. may as well
-// .Do(.. etc..)
-
-// TODO: add middleware thing.. like provider.With(etc..).BlockByNumber() ..
-
-// JSON-RPC methods..
-/*
-
-web3_clientVersion
-web3_sha3
-net_version
-net_listening
-net_peerCount
-eth_protocolVersion
-eth_syncing
-eth_coinbase
-eth_mining
-eth_hashrate
-eth_gasPrice
-eth_accounts
-eth_blockNumber
-eth_getBalance
-eth_getStorageAt
-eth_getTransactionCount
-eth_getBlockTransactionCountByHash
-eth_getBlockTransactionCountByNumber
-eth_getUncleCountByBlockHash
-eth_getUncleCountByBlockNumber
-eth_getCode
-eth_sign
-eth_signTransaction
-eth_sendTransaction
-eth_sendRawTransaction
-eth_call
-eth_estimateGas
-eth_getBlockByHash
-eth_getBlockByNumber
-eth_getTransactionByHash
-eth_getTransactionByBlockHashAndIndex
-eth_getTransactionByBlockNumberAndIndex
-eth_getTransactionReceipt
-eth_getUncleByBlockHashAndIndex
-eth_getUncleByBlockNumberAndIndex
-eth_getCompilers
-eth_compileSolidity
-eth_compileLLL
-eth_compileSerpent
-eth_newFilter
-eth_newBlockFilter
-eth_newPendingTransactionFilter
-eth_uninstallFilter
-eth_getFilterChanges
-eth_getFilterLogs
-eth_getLogs
-eth_getWork
-eth_submitWork
-eth_submitHashrate
-
-*/
+var _ Interface = (*Provider)(nil)
---
+++ ethrpc2/interface.go
@@ -0,0 +1,160 @@
+package ethrpc2
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/0xsequence/ethkit/go-ethereum"
+	"github.com/0xsequence/ethkit/go-ethereum/common"
+	"github.com/0xsequence/ethkit/go-ethereum/core/types"
+)
+
+// JSON-RPC methods..
+/*
+
+web3_clientVersion
+web3_sha3
+net_version
+net_listening
+net_peerCount
+eth_protocolVersion
+eth_syncing
+eth_coinbase
+eth_mining
+eth_hashrate
+eth_gasPrice
+eth_accounts
+eth_blockNumber
+eth_getBalance
+eth_getStorageAt
+eth_getTransactionCount
+eth_getBlockTransactionCountByHash
+eth_getBlockTransactionCountByNumber
+eth_getUncleCountByBlockHash
+eth_getUncleCountByBlockNumber
+eth_getCode
+eth_sign
+eth_signTransaction
+eth_sendTransaction
+eth_sendRawTransaction
+eth_call
+eth_estimateGas
+eth_getBlockByHash
+eth_getBlockByNumber
+eth_getTransactionByHash
+eth_getTransactionByBlockHashAndIndex
+eth_getTransactionByBlockNumberAndIndex
+eth_getTransactionReceipt
+eth_getUncleByBlockHashAndIndex
+eth_getUncleByBlockNumberAndIndex
+eth_getCompilers
+eth_compileSolidity
+eth_compileLLL
+eth_compileSerpent
+eth_newFilter
+eth_newBlockFilter
+eth_newPendingTransactionFilter
+eth_uninstallFilter
+eth_getFilterChanges
+eth_getFilterLogs
+eth_getLogs
+eth_getWork
+eth_submitWork
+eth_submitHashrate
+
+*/
+
+type Interface interface {
+	// ChainID = eth_chainId
+	ChainID(ctx context.Context) (*big.Int, error)
+
+	// BlockByHash = eth_getBlockByHash (true)
+	BlockByHash(ctx context.Context, hash common.Hash) (*types.Block, error)
+
+	// BlockByNumber = eth_getBlockByNumber (true)
+	BlockByNumber(ctx context.Context, number *big.Int) (*types.Block, error)
+
+	// BlockNumber = eth_blockNumber
+	BlockNumber(ctx context.Context) (uint64, error)
+
+	// PeerCount = net_peerCount
+	PeerCount(ctx context.Context) (uint64, error)
+
+	// HeaderByHash = eth_getBlockByHash (false)
+	HeaderByHash(ctx context.Context, hash common.Hash) (*types.Header, error)
+
+	// HeaderByNumber = eth_getBlockByHash (true)
+	HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error)
+
+	// TransactionByHash = eth_getTransactionByHash
+	TransactionByHash(ctx context.Context, hash common.Hash) (tx *types.Transaction, pending bool, err error)
+
+	// TransactionSender is a wrapper for eth_getTransactionByBlockHashAndIndex
+	TransactionSender(ctx context.Context, tx *types.Transaction, block common.Hash, index uint) (common.Address, error)
+
+	// TransactionCount = eth_getBlockTransactionCountByHash
+	TransactionCount(ctx context.Context, blockHash common.Hash) (uint, error)
+
+	// TransactionInBlock = eth_getTransactionByBlockHashAndIndex
+	TransactionInBlock(ctx context.Context, blockHash common.Hash, index uint) (*types.Transaction, error)
+
+	// TransactionReceipt = eth_getTransactionReceipt
+	TransactionReceipt(ctx context.Context, txHash common.Hash) (*types.Receipt, error)
+
+	// SyncProgress = eth_syncing
+	SyncProgress(ctx context.Context) (*ethereum.SyncProgress, error)
+
+	// NetworkID = net_version
+	NetworkID(ctx context.Context) (*big.Int, error)
+
+	// BalanceAt = eth_getBalance
+	BalanceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (*big.Int, error)
+
+	// StorageAt = eth_getStorageAt
+	StorageAt(ctx context.Context, account common.Address, key common.Hash, blockNumber *big.Int) ([]byte, error)
+
+	// NonceAt = eth_getTransactionCount
+	NonceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (uint64, error)
+
+	// FilterLogs = eth_getLogs
+	FilterLogs(ctx context.Context, q ethereum.FilterQuery) ([]types.Log, error)
+
+	// PendingBalanceAt = eth_getBalance ("pending")
+	PendingBalanceAt(ctx context.Context, account common.Address) (*big.Int, error)
+
+	// PendingStorageAt = eth_getStorageAt ("pending")
+	PendingStorageAt(ctx context.Context, account common.Address, key common.Hash) ([]byte, error)
+
+	// PendingCodeAt = eth_getCode ("pending")
+	PendingCodeAt(ctx context.Context, account common.Address) ([]byte, error)
+
+	// PendingNonceAt = eth_getTransactionCount ("pending")
+	PendingNonceAt(ctx context.Context, account common.Address) (uint64, error)
+
+	// PendingTransactionCount = eth_getBlockTransactionCountByNumber ("pending")
+	PendingTransactionCount(ctx context.Context) (uint, error)
+
+	// CallContract = eth_call (blockNumber)
+	CallContract(ctx context.Context, msg ethereum.CallMsg, blockNumber *big.Int) ([]byte, error)
+
+	// CallContractAtHash = eth_call (blockHash)
+	CallContractAtHash(ctx context.Context, msg ethereum.CallMsg, blockHash common.Hash) ([]byte, error)
+
+	// PendingCallContract = eth_call ("pending")
+	PendingCallContract(ctx context.Context, msg ethereum.CallMsg) ([]byte, error)
+
+	// SuggestGasPrice = eth_gasPrice
+	SuggestGasPrice(ctx context.Context) (*big.Int, error)
+
+	// SuggestGasTipCap = eth_maxPriorityFeePerGas
+	SuggestGasTipCap(ctx context.Context) (*big.Int, error)
+
+	// FeeHistory = eth_feeHistory
+	FeeHistory(ctx context.Context, blockCount uint64, lastBlock *big.Int, rewardPercentiles []float64) (*ethereum.FeeHistory, error)
+
+	// EstimateGas = eth_estimateGas
+	EstimateGas(ctx context.Context, msg ethereum.CallMsg) (uint64, error)
+
+	// SendTransaction = eth_sendRawTransaction
+	SendTransaction(ctx context.Context, tx *types.Transaction) error
+}
