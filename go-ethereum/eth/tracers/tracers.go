f273f91b92f82c1917df1a0eeb048977fe9011e7
poonai <rbalajis25@gmail.com>
2023-04-19 20:55:12 +0530

    remove execution results

--- go-ethereum/eth/tracers/tracers.go
+++ go-ethereum/eth/tracers/tracers.go
@@ -22,23 +22,3 @@ type TraceConfig struct {
 	// config are historically embedded in main object.
 	TracerConfig json.RawMessage
 }
-
-type ExecutionResult struct {
-	Gas         uint64         `json:"gas"`
-	Failed      bool           `json:"failed"`
-	ReturnValue string         `json:"returnValue"`
-	StructLogs  []StructLogRes `json:"structLogs"`
-}
-
-type StructLogRes struct {
-	Pc            uint64             `json:"pc"`
-	Op            string             `json:"op"`
-	Gas           uint64             `json:"gas"`
-	GasCost       uint64             `json:"gasCost"`
-	Depth         int                `json:"depth"`
-	Error         string             `json:"error,omitempty"`
-	Stack         *[]string          `json:"stack,omitempty"`
-	Memory        *[]string          `json:"memory,omitempty"`
-	Storage       *map[string]string `json:"storage,omitempty"`
-	RefundCounter uint64             `json:"refund,omitempty"`
-}
