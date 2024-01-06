4663b82f69021493a1bdf495dd180c2d60032947
Cesare Valitutto <cesare.valitutto@gmail.com>
2024-01-06 18:56:49 +0100

    feat(cli): add withdrawals

--- go-ethereum/core/types/withdrawal.go
+++ go-ethereum/core/types/withdrawal.go
@@ -29,10 +29,10 @@ import (
 
 // Withdrawal represents a validator withdrawal from the consensus layer.
 type Withdrawal struct {
-	Index     uint64         `json:"index"`          // monotonically increasing identifier issued by consensus layer
-	Validator uint64         `json:"validatorIndex"` // index of validator associated with withdrawal
+	Index     hexutil.Uint64 `json:"index"`          // monotonically increasing identifier issued by consensus layer
+	Validator hexutil.Uint64 `json:"validatorIndex"` // index of validator associated with withdrawal
 	Address   common.Address `json:"address"`        // target address for withdrawn ether
-	Amount    uint64         `json:"amount"`         // value of withdrawal in Gwei
+	Amount    hexutil.Uint64 `json:"amount"`         // value of withdrawal in Gwei
 }
 
 // field type overrides for gencodec
