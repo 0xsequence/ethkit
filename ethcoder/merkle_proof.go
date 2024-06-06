ef65d469ade696169fa256a87dd1ee2a2f27646c
Michael Standen <mstan@horizon.io>
2024-06-07 08:43:09 +1200

    Correct implementation

--- ethcoder/merkle_proof.go
+++ ethcoder/merkle_proof.go
@@ -2,17 +2,16 @@ package ethcoder
 
 import (
 	"bytes"
-	"crypto/sha256"
 	"errors"
 	"sort"
+
+	"github.com/0xsequence/ethkit/go-ethereum/common"
+	"github.com/0xsequence/ethkit/go-ethereum/crypto"
 )
 
-// NOTE: below is some ugly chatgpt ported code which we should refactor + clean up
-
-type TValue interface{}
+type TValue string
 type TLeaf []byte
 type TLayer []TLeaf
-type THashFn func(value TValue) TLeaf
 
 type Options struct {
 	SortLeaves bool
@@ -20,59 +19,40 @@ type Options struct {
 }
 
 type Proof struct {
-	Position string
+	IsLeft   bool
 	Data     TLeaf
 }
 
 type MerkleTree struct {
-	HashFn     THashFn
-	Leaves     []TLeaf
-	Layers     []TLayer
-	SortLeaves bool
-	SortPairs  bool
+	leaves     []TLeaf
+	layers     []TLayer
+	sortLeaves bool
+	sortPairs  bool
 }
 
 func NewMerkleTree(leaves []TLeaf, options Options) *MerkleTree {
 	mt := &MerkleTree{
-		HashFn:     bufferifyFn(sha256Hash),
-		SortLeaves: options.SortLeaves,
-		SortPairs:  options.SortPairs,
+		sortLeaves: options.SortLeaves,
+		sortPairs:  options.SortPairs,
 	}
 	mt.processLeaves(leaves)
 	return mt
 }
 
-func bufferifyFn(hashFn func(data []byte) []byte) THashFn {
-	return func(value TValue) TLeaf {
-		switch v := value.(type) {
-		case string:
-			return hashFn([]byte(v))
-		case []byte:
-			return hashFn(v)
-		default:
-			return nil
-		}
-	}
-}
-
-func sha256Hash(data []byte) []byte {
-	hash := sha256.Sum256(data)
-	return hash[:]
-}
-
 func (mt *MerkleTree) processLeaves(leaves []TLeaf) {
-	mt.Leaves = make([]TLeaf, len(leaves))
-	copy(mt.Leaves, leaves)
-	if mt.SortLeaves {
-		sort.Slice(mt.Leaves, func(i, j int) bool {
-			return bytes.Compare(mt.Leaves[i], mt.Leaves[j]) < 0
+	mt.leaves = make([]TLeaf, len(leaves))
+	copy(mt.leaves, leaves)
+	if mt.sortLeaves {
+		sort.Slice(mt.leaves, func(i, j int) bool {
+			return bytes.Compare(mt.leaves[i], mt.leaves[j]) < 0
 		})
 	}
-	mt.createHashes(mt.Leaves)
+	mt.createHashes(mt.leaves)
 }
 
 func (mt *MerkleTree) createHashes(nodes []TLeaf) {
-	mt.Layers = append(mt.Layers, nodes)
+	mt.layers = make([]TLayer, 0)
+	mt.layers = append(mt.layers, nodes)
 	for len(nodes) > 1 {
 		var nextLayer []TLeaf
 		for i := 0; i < len(nodes); i += 2 {
@@ -81,28 +61,28 @@ func (mt *MerkleTree) createHashes(nodes []TLeaf) {
 			} else {
 				left := nodes[i]
 				right := nodes[i+1]
-				if mt.SortPairs && bytes.Compare(left, right) > 0 {
+				if mt.sortPairs && bytes.Compare(left, right) > 0 {
 					left, right = right, left
 				}
-				hash := mt.HashFn(append(left, right...))
+				hash := crypto.Keccak256(append(left, right...))
 				nextLayer = append(nextLayer, hash)
 			}
 		}
 		nodes = nextLayer
-		mt.Layers = append(mt.Layers, nodes)
+		mt.layers = append(mt.layers, nodes)
 	}
 }
 
-func (mt *MerkleTree) GetRoot() TLeaf {
-	if len(mt.Layers) == 0 {
+func (mt *MerkleTree) GetRoot() []byte {
+	if len(mt.layers) == 0 {
 		return TLeaf{}
 	}
-	return mt.Layers[len(mt.Layers)-1][0]
+	return mt.layers[len(mt.layers)-1][0]
 }
 
 func (mt *MerkleTree) GetProof(leaf TLeaf) ([]Proof, error) {
 	leafIndex := -1
-	for i, l := range mt.Leaves {
+	for i, l := range mt.leaves {
 		if bytes.Equal(l, leaf) {
 			leafIndex = i
 			break
@@ -111,17 +91,15 @@ func (mt *MerkleTree) GetProof(leaf TLeaf) ([]Proof, error) {
 	if leafIndex == -1 {
 		return nil, errors.New("leaf not found in tree")
 	}
+
 	proof := []Proof{}
-	for i := 0; i < len(mt.Layers)-1; i++ {
-		layer := mt.Layers[i]
+	for i := 0; i < len(mt.layers)-1; i++ {
+		layer := mt.layers[i]
 		pairIndex := leafIndex ^ 1
 		if pairIndex < len(layer) {
-			position := "left"
-			if leafIndex%2 == 0 {
-				position = "right"
-			}
+			isLeft := leafIndex%2 != 0
 			proof = append(proof, Proof{
-				Position: position,
+				IsLeft:   isLeft,
 				Data:     layer[pairIndex],
 			})
 		}
@@ -130,16 +108,49 @@ func (mt *MerkleTree) GetProof(leaf TLeaf) ([]Proof, error) {
 	return proof, nil
 }
 
-func (mt *MerkleTree) Verify(proof []Proof, targetNode, root TLeaf) bool {
-	hash := targetNode
-	for _, node := range proof {
-		var data []byte
-		if node.Position == "left" {
-			data = append(node.Data, hash...)
-		} else {
-			data = append(hash, node.Data...)
-		}
-		hash = mt.HashFn(data)
+func (mt *MerkleTree) GetHexProof(leaf TLeaf) []common.Hash {
+	proof, _ := mt.GetProof(leaf)
+	hexProof := make([]common.Hash, len(proof))
+	for _, p := range proof {
+		hexProof = append(hexProof, common.BytesToHash(p.Data))
 	}
-	return bytes.Equal(hash, root)
+	return hexProof
+}
+
+func (mt *MerkleTree) Verify(proof []Proof, targetNode, root []byte) (bool, error) {
+	hash := targetNode
+
+	if proof == nil || len(targetNode) == 0 || len(root) == 0 {
+			return false, nil
+	}
+
+	for i := 0; i < len(proof); i++ {
+			node := proof[i]
+			var data []byte
+			var isLeftNode bool
+
+			data = node.Data
+			isLeftNode = node.IsLeft
+
+			var buffers [][]byte
+
+			if mt.sortPairs {
+					if bytes.Compare(hash, data) < 0 {
+							buffers = append(buffers, hash, data)
+					} else {
+							buffers = append(buffers, data, hash)
+					}
+					hash = crypto.Keccak256(bytes.Join(buffers, []byte{}))
+			} else {
+					buffers = append(buffers, hash)
+					if isLeftNode {
+							buffers = append([][]byte{data}, buffers...)
+					} else {
+							buffers = append(buffers, data)
+					}
+					hash = crypto.Keccak256(bytes.Join(buffers, []byte{}))
+			}
+	}
+
+	return bytes.Equal(hash, root), nil
 }
--- ethcoder/merkle_proof_test.go
+++ ethcoder/merkle_proof_test.go
@@ -1,27 +1,110 @@
 package ethcoder
 
-import "testing"
+import (
+	"crypto/rand"
+	"fmt"
+	"testing"
 
-func TestMerkleProof(t *testing.T) {
-	// 	leaves := []TLeaf{
-	// 		sha256Hash([]byte("a")),
-	// 		sha256Hash([]byte("b")),
-	// 		sha256Hash([]byte("c")),
-	// 		sha256Hash([]byte("d")),
-	// 	}
-	// 	mt := NewMerkleTree(leaves, Options{SortLeaves: false, SortPairs: false})
-	// 	root := mt.GetRoot()
-	// 	fmt.Printf("Root: %x\n", root)
+	"github.com/0xsequence/ethkit/go-ethereum/common"
+	"github.com/stretchr/testify/assert"
+)
 
-	// 	proof, err := mt.GetProof(sha256Hash([]byte("a")))
-	// 	if err != nil {
-	// 		fmt.Println("Error:", err)
-	// 		return
-	// 	}
-	// 	for _, p := range proof {
-	// 		fmt.Printf("Proof: Position=%s, Data=%x\n", p.Position, p.Data)
-	// 	}
+func TestMerkleProofKnown(t *testing.T) {
+	testAddr := common.HexToAddress("0x1e946c284bdBb05Fb6EF41016C524E8681e3d05E")
+	leaves := []TLeaf{
+		testAddr.Bytes(),
+		common.HexToAddress("0x1D74B866598B339006160d704642459B04ba890B").Bytes(),
+		common.HexToAddress("0x37e948435E916069D3a1431Ddf508421073fF3E7").Bytes(),
+		common.HexToAddress("0x29c34A7d23B8BCBE7c5Ec94C6525b78bb5cbAf36").Bytes(),
+	}
+	mt := NewMerkleTree(leaves, Options{SortLeaves: true, SortPairs: true})
 
-	// isValid := mt.Verify(proof, sha256Hash([]byte("a")), root)
-	// fmt.Printf("Is valid: %v\n", isValid)
+	expectedRoot := common.Hex2Bytes("2620d31912c95198ebbf40473b7b069e98587ec49d0cd46aacef8c746c682334")
+	root := mt.GetRoot()
+	assert.Equal(t, expectedRoot, root)
+	fmt.Printf("Root: %x\n", root)
+
+	expectedProof := [][]byte{
+		common.Hex2Bytes("1d74b866598b339006160d704642459b04ba890b"),
+		common.Hex2Bytes("39ceb165765d969b9bfbbab524649adc484bab29db86b6c0df8635feebf0154e"),
+	}
+	proof, err := mt.GetProof(testAddr.Bytes())
+	assert.Nil(t, err)
+	for i, p := range proof {
+		fmt.Printf("Proof part %d: IsLeft=%v, Data=%x\n", i, p.IsLeft, p.Data)
+		assert.Equal(t, expectedProof[i], []byte(p.Data))
+	}
+
+	isValid, err := mt.Verify(proof, testAddr.Bytes(), root)
+	assert.Nil(t, err)
+	assert.True(t, isValid)
+}
+
+func TestMerkleProofLarge(t *testing.T) {
+	addrCount := 100
+	leaves := make([]TLeaf, addrCount)
+	for i := 0; i < addrCount; i++ {
+		leaf := make([]byte, 20)
+		rand.Read(leaf)
+		leaves[i] = leaf
+	}
+
+	mt := NewMerkleTree(leaves, Options{SortLeaves: true, SortPairs: true})
+
+	root := mt.GetRoot()
+	assert.NotNil(t, root)
+
+	proof, err := mt.GetProof(leaves[69])
+	assert.Nil(t, err)
+	assert.GreaterOrEqual(t, len(proof), 1)
+
+	isValid, err := mt.Verify(proof, leaves[69], root)
+	assert.Nil(t, err)
+	assert.True(t, isValid)
+}
+
+func TestMerkleInvalidLeaf(t *testing.T) {
+	invalidLeaf := common.HexToAddress("0x1e946c284bdBb05Fb6EF41016C524E8681e3d05E").Bytes()
+	leaves := []TLeaf{
+		common.HexToAddress("0x1D74B866598B339006160d704642459B04ba890B").Bytes(),
+		common.HexToAddress("0x37e948435E916069D3a1431Ddf508421073fF3E7").Bytes(),
+		common.HexToAddress("0x29c34A7d23B8BCBE7c5Ec94C6525b78bb5cbAf36").Bytes(),
+	}
+
+	mt := NewMerkleTree(leaves, Options{SortLeaves: true, SortPairs: true})
+
+	root := mt.GetRoot()
+	assert.NotNil(t, root)
+
+	// Invalid leaf
+	_, err := mt.GetProof(invalidLeaf)
+	assert.Error(t, err)
+
+	// Valid proof
+	proof, err := mt.GetProof(leaves[0])
+	assert.Nil(t, err)
+
+	// Invalid leaf
+	isValid, err := mt.Verify(proof, invalidLeaf, root)
+	assert.Error(t, err)
+	assert.False(t, isValid)
+}
+
+func TestMerkleSingleLeaf(t *testing.T) {
+	leaf := common.HexToAddress("0x1e946c284bdBb05Fb6EF41016C524E8681e3d05E").Bytes()
+	leaves := []TLeaf{
+		leaf,
+	}
+
+	mt := NewMerkleTree(leaves, Options{SortLeaves: true, SortPairs: true})
+
+	root := mt.GetRoot()
+	assert.NotNil(t, root)
+
+	proof, err := mt.GetProof(leaf)
+	assert.Nil(t, err)
+
+	isValid, err := mt.Verify(proof, leaf, root)
+	assert.Nil(t, err)
+	assert.True(t, isValid)
 }
