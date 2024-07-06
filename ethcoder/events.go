b0bf10c8f1411af7c9281b465b029f2e5822d19f
Peter Kieltyka <peter.kieltyka@gmail.com>
2024-07-06 16:41:08 -0400

    ethcoder: new log event decoding functions

--- ethcoder/events.go
+++ ethcoder/events.go
@@ -5,41 +5,210 @@ import (
 	"strings"
 
 	"github.com/0xsequence/ethkit"
+	"github.com/0xsequence/ethkit/go-ethereum/accounts/abi"
+	"github.com/0xsequence/ethkit/go-ethereum/accounts/abi/bind"
+	"github.com/0xsequence/ethkit/go-ethereum/common"
+	"github.com/0xsequence/ethkit/go-ethereum/core/types"
 )
 
 // EventTopicHash returns the keccak256 hash of the event signature
 //
 // e.g. "Transfer(address indexed from, address indexed to, uint256 value)"
 // will return 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef
-func EventTopicHash(event string) (ethkit.Hash, error) {
-	eventSig := parseEventSignature(event)
-	if eventSig == "" {
-		return ethkit.Hash{}, fmt.Errorf("ethcoder: event format is invalid, expecting Method(arg1,arg2,..)")
+func EventTopicHash(event string) (ethkit.Hash, string, error) {
+	eventDef, err := ParseEventDef(event)
+	if err != nil {
+		return ethkit.Hash{}, "", fmt.Errorf("ethcoder: %w", err)
 	}
-	return Keccak256Hash([]byte(eventSig)), nil
+	topicHash := common.HexToHash(eventDef.TopicHash)
+	return topicHash, eventDef.Sig, nil
 }
 
-func parseEventSignature(event string) string {
+type EventDef struct {
+	TopicHash string   `json:"topicHash"` // the event topic hash, ie. 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef
+	Name      string   `json:"name"`      // the event name, ie. Transfer
+	Sig       string   `json:"sig"`       // the event sig, ie. Transfer(address,address,uint256)
+	ArgTypes  []string `json:"argTypes"`  // the event arg types, ie. [address, address, uint256]
+	ArgNames  []string `json:"argNames"`  // the event arg names, ie. [from, to, value] or ["","",""]
+}
+
+func ParseEventDef(event string) (EventDef, error) {
+	eventDef := EventDef{
+		ArgTypes: []string{},
+		ArgNames: []string{},
+	}
+
+	var errInvalid = fmt.Errorf("event format is invalid, expecting Method(arg1,arg2,..)")
+
 	if !strings.Contains(event, "(") || !strings.Contains(event, ")") {
-		return ""
+		return eventDef, errInvalid
 	}
 	p := strings.Split(event, "(")
 	if len(p) != 2 {
-		return ""
+		return eventDef, errInvalid
 	}
-	method := p[0]
+	method := strings.TrimSpace(p[0])
+	eventDef.Name = method
 
 	args := strings.TrimSuffix(p[1], ")")
 	if args == "" {
-		return fmt.Sprintf("%s()", method)
+		eventDef.Sig = fmt.Sprintf("%s()", method)
+	} else {
+		typs := []string{}
+		names := []string{}
+
+		p = strings.Split(args, ",")
+		for _, a := range p {
+			arg := strings.Split(strings.TrimSpace(a), " ")
+
+			typ := arg[0]
+			typs = append(typs, typ)
+
+			var name string
+			if len(arg) > 1 {
+				name = arg[len(arg)-1]
+			}
+			names = append(names, name)
+		}
+
+		eventDef.ArgTypes = typs
+		eventDef.ArgNames = names
+
+		eventDef.Sig = fmt.Sprintf("%s(%s)", method, strings.Join(typs, ","))
 	}
 
+	eventDef.TopicHash = Keccak256Hash([]byte(eventDef.Sig)).String()
+
+	return eventDef, nil
+}
+
+// ..
+func DecodeTransactionLogByContractABIJSON(txnLog types.Log, contractABIJSON string) (EventDef, []interface{}, bool, error) {
+	contractABI, err := abi.JSON(strings.NewReader(contractABIJSON))
+	if err != nil {
+		return EventDef{}, nil, false, fmt.Errorf("invalid contract ABI definition: %w", err)
+	}
+
+	return DecodeTransactionLogByContractABI(txnLog, contractABI)
+}
+
+// ..
+func DecodeTransactionLogByContractABI(txnLog types.Log, contractABI abi.ABI) (EventDef, []interface{}, bool, error) {
+	eventDef := EventDef{}
+	topicHash := txnLog.Topics[0]
+	eventDef.TopicHash = topicHash.String()
+
+	abiEvent, err := contractABI.EventByID(topicHash)
+	if err != nil {
+		return EventDef{}, nil, false, nil
+	}
+
+	eventDef.Name = abiEvent.Name
+
+	args := []string{}
 	typs := []string{}
-	p = strings.Split(args, ",")
-	for _, a := range p {
-		typ := strings.Split(strings.TrimSpace(a), " ")[0]
-		typs = append(typs, typ)
+	for _, arg := range abiEvent.Inputs {
+		args = append(args, arg.Name)
+		typs = append(typs, arg.Type.String())
+	}
+	eventDef.ArgNames = args
+	eventDef.ArgTypes = typs
+
+	bc := bind.NewBoundContract(txnLog.Address, contractABI, nil, nil, nil)
+
+	eventMap := map[string]interface{}{}
+	err = bc.UnpackLogIntoMap(eventMap, abiEvent.Name, txnLog)
+	if err != nil {
+		return EventDef{}, nil, false, fmt.Errorf("DecodeLogEventByContractABI: %w", err)
 	}
 
-	return fmt.Sprintf("%s(%s)", method, strings.Join(typs, ","))
+	eventDef.Sig = fmt.Sprintf("%s(%s)", eventDef.Name, strings.Join(typs, ","))
+
+	eventValues := []interface{}{}
+	for _, arg := range args {
+		eventValues = append(eventValues, eventMap[arg])
+	}
+
+	return eventDef, eventValues, true, nil
+}
+
+func DecodeTransactionLogByEventSig(txnLog types.Log, eventSig string, returnHexValues bool) (EventDef, []interface{}, bool, error) {
+	eventDef, err := ParseEventDef(eventSig)
+	if err != nil {
+		return eventDef, nil, false, fmt.Errorf("ParseEventDef: %w", err)
+	}
+
+	// Lets build a mini abi on-demand, and decode it
+	abiArgs := abi.Arguments{}
+	numIndexedArgs := len(txnLog.Topics) - 1
+
+	for i, argType := range eventDef.ArgTypes {
+		argName := eventDef.ArgNames[i]
+		if argName == "" {
+			argName = fmt.Sprintf("arg%d", i)
+		}
+
+		typ, err := abi.NewType(argType, "", nil)
+		if err != nil {
+			return eventDef, nil, false, fmt.Errorf("invalid abi argument type '%s': %w", argType, err)
+		}
+
+		abiArgs = append(abiArgs, abi.Argument{Name: argName, Type: typ, Indexed: i < numIndexedArgs})
+	}
+
+	if !returnHexValues {
+
+		// Decode into native runtime types
+		abiEvent := abi.NewEvent(eventDef.Name, eventDef.Name, false, abiArgs)
+		contractABI := abi.ABI{
+			Events: map[string]abi.Event{},
+		}
+		contractABI.Events[eventDef.Name] = abiEvent
+
+		args := []string{}
+		for _, arg := range abiEvent.Inputs {
+			args = append(args, arg.Name)
+		}
+
+		bc := bind.NewBoundContract(txnLog.Address, contractABI, nil, nil, nil)
+
+		eventMap := map[string]interface{}{}
+		err = bc.UnpackLogIntoMap(eventMap, abiEvent.Name, txnLog)
+		if err != nil {
+			return eventDef, nil, false, fmt.Errorf("UnpackLogIntoMap: %w", err)
+		}
+
+		eventValues := []interface{}{}
+		for _, arg := range args {
+			eventValues = append(eventValues, eventMap[arg])
+		}
+
+		return eventDef, eventValues, true, nil
+
+	} else {
+		// Decode into hex values, which means []interface{} will always return array of strings.
+		// This is useful in cases when you want to return the hex values of the values instead
+		// of decoding to runtime types.
+
+		eventValues := []interface{}{}
+		dataPos := 0
+
+		for i, arg := range abiArgs {
+			if arg.Indexed {
+				byteSize := abi.GetTypeSize(arg.Type)
+				if byteSize > arg.Type.Size {
+					byteSize = arg.Type.Size // for case of address type
+				}
+				data := txnLog.Topics[i+1].Bytes()[32-byteSize:]
+				eventValues = append(eventValues, HexEncode(data))
+			} else {
+				byteSize := abi.GetTypeSize(arg.Type)
+				data := txnLog.Data[dataPos : dataPos+byteSize]
+				dataPos += byteSize
+				eventValues = append(eventValues, HexEncode(data))
+			}
+		}
+
+		return eventDef, eventValues, true, nil
+	}
 }
--- ethcoder/events_test.go
+++ ethcoder/events_test.go
@@ -1,9 +1,13 @@
 package ethcoder_test
 
 import (
+	"math/big"
 	"testing"
 
 	"github.com/0xsequence/ethkit/ethcoder"
+	"github.com/0xsequence/ethkit/go-ethereum/common"
+	"github.com/0xsequence/ethkit/go-ethereum/common/hexutil"
+	"github.com/0xsequence/ethkit/go-ethereum/core/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -14,11 +18,208 @@ func TestEventTopicHash(t *testing.T) {
 		{"Transfer(address indexed from, address indexed to, uint256 value)"},
 		{"Transfer(address from, address indexed to, uint256 value)"},
 		{"Transfer(address, address , uint256 )"},
+		{"Transfer   (address from, address , uint256 value)"},
 	}
 
 	for _, x := range in {
-		topicHash, err := ethcoder.EventTopicHash(x.event)
+		topicHash, eventSig, err := ethcoder.EventTopicHash(x.event)
 		require.NoError(t, err)
 		require.Equal(t, "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef", topicHash.String())
+		require.Equal(t, "Transfer(address,address,uint256)", eventSig)
 	}
+
+	for _, x := range in {
+		eventDef, err := ethcoder.ParseEventDef(x.event)
+		require.NoError(t, err)
+		require.Equal(t, "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef", eventDef.TopicHash)
+		require.Equal(t, "Transfer", eventDef.Name)
+		require.Equal(t, "Transfer(address,address,uint256)", eventDef.Sig)
+		require.Equal(t, []string{"address", "address", "uint256"}, eventDef.ArgTypes)
+		// require.Equal(t, []string{"from", "to", "value"}, eventDef.ArgNames)
+	}
+}
+
+func TestDecodeTransactionLogByContractABIJSON(t *testing.T) {
+	logTopics := []string{
+		"0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+		"0x00000000000000000000000037af74b8096a6fd85bc4a36653a60b8d673baefc",
+		"0x000000000000000000000000ba12222222228d8ba445958a75a0704d566bf2c8",
+	}
+	logData := "0x0000000000000000000000000000000000000000000000000000000002b46676"
+
+	txnLog := types.Log{}
+	txnLog.Topics = []common.Hash{}
+
+	for _, topic := range logTopics {
+		txnLog.Topics = append(txnLog.Topics, common.HexToHash(topic))
+	}
+	txnLog.Data, _ = hexutil.Decode(logData)
+
+	var erc20ABI = "[{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"owner\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"spender\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"value\",\"type\":\"uint256\"}],\"name\":\"Approval\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"from\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"value\",\"type\":\"uint256\"}],\"name\":\"Transfer\",\"type\":\"event\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"owner\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"spender\",\"type\":\"address\"}],\"name\":\"allowance\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"spender\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"approve\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"account\",\"type\":\"address\"}],\"name\":\"balanceOf\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"totalSupply\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"recipient\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"transfer\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"sender\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"recipient\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"transferFrom\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
+
+	eventDef, eventValues, ok, err := ethcoder.DecodeTransactionLogByContractABIJSON(txnLog, erc20ABI)
+	require.NoError(t, err)
+	require.True(t, ok)
+	require.Equal(t, "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef", eventDef.TopicHash)
+	require.Equal(t, "Transfer", eventDef.Name)
+	require.Equal(t, "Transfer(address,address,uint256)", eventDef.Sig)
+	require.Equal(t, []string{"from", "to", "value"}, eventDef.ArgNames)
+
+	require.Equal(t, common.HexToAddress("0x37af74b8096a6fd85bc4a36653a60b8d673baefc"), eventValues[0])
+	require.Equal(t, common.HexToAddress("0xba12222222228d8ba445958a75a0704d566bf2c8"), eventValues[1])
+	require.Equal(t, big.NewInt(45377142), eventValues[2])
+	// spew.Dump(eventValues)
+}
+
+func TestDecodeTransactionLogByEventSig1(t *testing.T) {
+	logTopics := []string{
+		"0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+		"0x00000000000000000000000037af74b8096a6fd85bc4a36653a60b8d673baefc",
+		"0x000000000000000000000000ba12222222228d8ba445958a75a0704d566bf2c8",
+	}
+	logData := "0x0000000000000000000000000000000000000000000000000000000002b46676"
+
+	txnLog := types.Log{}
+	txnLog.Topics = []common.Hash{}
+
+	for _, topic := range logTopics {
+		txnLog.Topics = append(txnLog.Topics, common.HexToHash(topic))
+	}
+	txnLog.Data, _ = hexutil.Decode(logData)
+
+	var eventSig = "Transfer(address,address,uint256)"
+
+	eventDef, eventValues, ok, err := ethcoder.DecodeTransactionLogByEventSig(txnLog, eventSig, false)
+	require.NoError(t, err)
+	require.True(t, ok)
+	require.Equal(t, "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef", eventDef.TopicHash)
+	require.Equal(t, "Transfer", eventDef.Name)
+	require.Equal(t, "Transfer(address,address,uint256)", eventDef.Sig)
+	require.Equal(t, []string{"", "", ""}, eventDef.ArgNames)
+	require.Equal(t, common.HexToAddress("0x37af74b8096a6fd85bc4a36653a60b8d673baefc"), eventValues[0])
+	require.Equal(t, common.HexToAddress("0xba12222222228d8ba445958a75a0704d566bf2c8"), eventValues[1])
+	require.Equal(t, big.NewInt(45377142), eventValues[2])
+	// spew.Dump(eventValues)
+
+	eventDef, eventValues, ok, err = ethcoder.DecodeTransactionLogByEventSig(txnLog, eventSig, true)
+	require.NoError(t, err)
+	require.True(t, ok)
+	require.Equal(t, "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef", eventDef.TopicHash)
+	require.Equal(t, "Transfer", eventDef.Name)
+	require.Equal(t, "Transfer(address,address,uint256)", eventDef.Sig)
+	require.Equal(t, []string{"", "", ""}, eventDef.ArgNames)
+	require.Equal(t, "0x37af74b8096a6fd85bc4a36653a60b8d673baefc", eventValues[0])
+	require.Equal(t, "0xba12222222228d8ba445958a75a0704d566bf2c8", eventValues[1])
+	require.Equal(t, "0x0000000000000000000000000000000000000000000000000000000002b46676", eventValues[2])
+	// spew.Dump(eventValues)
+}
+
+func TestDecodeTransactionLogByEventSig2(t *testing.T) {
+	logTopics := []string{
+		"0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+		"0x0000000000000000000000000000000000000000000000000000000000000000",
+		"0x0000000000000000000000001a05955180488cb07db065d174b44df9aeb0fdb1",
+		"0x000000000000000000000000000000000000000000000000000000000004d771",
+	}
+	logData := "0x"
+
+	txnLog := types.Log{}
+	txnLog.Topics = []common.Hash{}
+
+	for _, topic := range logTopics {
+		txnLog.Topics = append(txnLog.Topics, common.HexToHash(topic))
+	}
+	txnLog.Data, _ = hexutil.Decode(logData)
+
+	var eventSig = "Transfer(address,address,uint256)"
+
+	eventDef, eventValues, ok, err := ethcoder.DecodeTransactionLogByEventSig(txnLog, eventSig, true)
+	require.NoError(t, err)
+	require.True(t, ok)
+	require.Equal(t, "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef", eventDef.TopicHash)
+	require.Equal(t, "Transfer", eventDef.Name)
+	require.Equal(t, "Transfer(address,address,uint256)", eventDef.Sig)
+	require.Equal(t, []string{"", "", ""}, eventDef.ArgNames)
+	require.Equal(t, "0x0000000000000000000000000000000000000000", eventValues[0])
+	require.Equal(t, "0x1a05955180488cb07db065d174b44df9aeb0fdb1", eventValues[1])
+	require.Equal(t, "0x000000000000000000000000000000000000000000000000000000000004d771", eventValues[2])
+	// spew.Dump(eventValues)
+}
+
+func TestDecodeTransactionLogByEventSig3(t *testing.T) {
+	logTopics := []string{
+		"0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62",
+		"0x000000000000000000000000d91e80cf2e7be2e162c6513ced06f1dd0da35296",
+		"0x0000000000000000000000004ce73141dbfce41e65db3723e31059a730f0abad",
+		"0x000000000000000000000000c5d563a36ae78145c45a50134d48a1215220f80a",
+	}
+	logData := "0xa08c15ba3595b44412ba290036a59015de859621fede8d4f2b9965f9956beca30000000000000000000000000000000000000000000000000000000000501bd0"
+
+	txnLog := types.Log{}
+	txnLog.Topics = []common.Hash{}
+
+	for _, topic := range logTopics {
+		txnLog.Topics = append(txnLog.Topics, common.HexToHash(topic))
+	}
+	txnLog.Data, _ = hexutil.Decode(logData)
+
+	var eventSig = "TransferSingle(address,address,address,uint256,uint256)"
+
+	eventDef, eventValues, ok, err := ethcoder.DecodeTransactionLogByEventSig(txnLog, eventSig, true)
+	require.NoError(t, err)
+	require.True(t, ok)
+	require.Equal(t, "0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62", eventDef.TopicHash)
+	require.Equal(t, "TransferSingle", eventDef.Name)
+	require.Equal(t, "TransferSingle(address,address,address,uint256,uint256)", eventDef.Sig)
+	require.Equal(t, []string{"", "", "", "", ""}, eventDef.ArgNames)
+	require.Equal(t, "0xd91e80cf2e7be2e162c6513ced06f1dd0da35296", eventValues[0])
+	require.Equal(t, "0x4ce73141dbfce41e65db3723e31059a730f0abad", eventValues[1])
+	require.Equal(t, "0xc5d563a36ae78145c45a50134d48a1215220f80a", eventValues[2])
+	require.Equal(t, "0xa08c15ba3595b44412ba290036a59015de859621fede8d4f2b9965f9956beca3", eventValues[3])
+	require.Equal(t, "0x0000000000000000000000000000000000000000000000000000000000501bd0", eventValues[4])
+	// spew.Dump(eventValues)
+}
+
+func TestDecodeTransactionLogByEventSig4(t *testing.T) {
+	logTopics := []string{
+		"0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67",
+		"0x000000000000000000000000ec7be89e9d109e7e3fec59c222cf297125fefda2",
+		"0x000000000000000000000000ec7be89e9d109e7e3fec59c222cf297125fefda2",
+	}
+	logData := "0x0000000000000000000000000000000000000000000000100f4b6d6675790000fffffffffffffffffffffffffffffffffffffffffffffffffffffffff7271967000000000000000000000000000000000000000000000be0c951878517d91842000000000000000000000000000000000000000000000000233dca2396037eaefffffffffffffffffffffffffffffffffffffffffffffffffffffffffffbada1"
+
+	txnLog := types.Log{}
+	txnLog.Topics = []common.Hash{}
+
+	for _, topic := range logTopics {
+		txnLog.Topics = append(txnLog.Topics, common.HexToHash(topic))
+	}
+	txnLog.Data, _ = hexutil.Decode(logData)
+
+	var eventSig = "Swap (address sender, address recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)"
+
+	eventDef, eventValues, ok, err := ethcoder.DecodeTransactionLogByEventSig(txnLog, eventSig, true) // use generics...?
+	require.NoError(t, err)
+	require.True(t, ok)
+	require.Equal(t, "0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67", eventDef.TopicHash)
+	require.Equal(t, "Swap", eventDef.Name)
+	require.Equal(t, "Swap(address,address,int256,int256,uint160,uint128,int24)", eventDef.Sig)
+	require.Equal(t, []string{"sender", "recipient", "amount0", "amount1", "sqrtPriceX96", "liquidity", "tick"}, eventDef.ArgNames)
+	require.Equal(t, "0xec7be89e9d109e7e3fec59c222cf297125fefda2", eventValues[0])
+	require.Equal(t, "0xec7be89e9d109e7e3fec59c222cf297125fefda2", eventValues[1])
+	require.Equal(t, "0x0000000000000000000000000000000000000000000000100f4b6d6675790000", eventValues[2])
+	require.Equal(t, "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffff7271967", eventValues[3])
+	require.Equal(t, "0x000000000000000000000000000000000000000000000be0c951878517d91842", eventValues[4])
+	require.Equal(t, "0x000000000000000000000000000000000000000000000000233dca2396037eae", eventValues[5])
+	require.Equal(t, "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffbada1", eventValues[6])
+	// spew.Dump(eventValues)
+
+	dataCheck := ""
+	for i := 2; i < len(eventValues); i++ {
+		v := eventValues[i]
+		s := v.(string)
+		dataCheck += s[2:]
+	}
+	dataCheck = "0x" + dataCheck
+	require.Equal(t, logData, dataCheck)
 }
