dc519bb991646a26cbdb2b1475469adc98256c83
Peter Kieltyka <peter.kieltyka@gmail.com>
2024-07-12 19:06:34 -0400

    ethcoder: event, parse named args

--- ethcoder/events_parser.go
+++ ethcoder/events_parser.go
@@ -45,12 +45,13 @@ func ParseEventDef(event string) (EventDef, error) {
 		eventDef.Sig = fmt.Sprintf("%s()", method)
 	} else {
 		// event parser
-		tree, err := parseEventArgs(args)
+		tree, err := parseEventArgs(args, 0)
 		if err != nil {
 			return eventDef, err
 		}
 
-		argsSig, typs, indexed, err := groupEventSelectorTree(tree, true)
+		// TODO ...
+		argsSig, typs, indexed, _, err := groupEventSelectorTree(tree, true)
 		if err != nil {
 			return eventDef, err
 		}
@@ -70,18 +71,22 @@ func ParseEventDef(event string) (EventDef, error) {
 type eventSelectorTree struct {
 	left       string
 	indexed    []bool
+	names      []string
 	tuple      []eventSelectorTree
 	tupleArray string
-	right      []eventSelectorTree
+	// tupleIndexed bool ?
+	// tupleName string ? ..
+	right []eventSelectorTree
 }
 
 // parseEventArgs parses the event arguments and returns a tree structure
 // ie. "address indexed from, address indexed to, uint256 value".
-//
-// TODO: right now we just parse the argument types, but we should
-// also parse out the indexed flag and also the names.
-func parseEventArgs(eventArgs string) (eventSelectorTree, error) {
+func parseEventArgs(eventArgs string, iteration int) (eventSelectorTree, error) {
 	args := strings.TrimSpace(eventArgs)
+	if iteration == 0 {
+		args = strings.ReplaceAll(eventArgs, "  ", "") // ..????
+	}
+
 	out := eventSelectorTree{}
 	if args == "" {
 		return out, nil
@@ -97,6 +102,7 @@ func parseEventArgs(eventArgs string) (eventSelectorTree, error) {
 	p2 := ""
 	p2ar := ""
 	p2indexed := false
+	p2name := ""
 	p3 := ""
 
 	if a < 0 {
@@ -122,14 +128,29 @@ func parseEventArgs(eventArgs string) (eventSelectorTree, error) {
 		// remove end params
 		x1 := strings.LastIndex(p2, "]")
 		x2 := strings.LastIndex(p2, ")")
-		if strings.LastIndex(p2, "indexed") > x1 || strings.LastIndex(p2, "indexed") > x2 {
+		xx := max(x1, x2)
+		// if strings.LastIndex(p2, "indexed") > x1 || strings.LastIndex(p2, "indexed") > x2 {
+		// 	p2indexed = true
+		// }
+		// fmt.Println("!!!WEEE!", p2)
+
+		// get indexed/var name from end
+		n := strings.Split(strings.TrimSpace(p2[xx+1:]), " ")
+		if len(n) == 1 {
+			p2name = strings.TrimRight(n[0], ",")
+		} else if len(n) == 2 && n[0] == "indexed" {
 			p2indexed = true
+			p2name = strings.TrimRight(n[1], ",")
 		}
-		if x1 > x2 {
-			p2 = p2[:x1+1]
-		} else {
-			p2 = p2[:x2+1]
-		}
+		// spew.Dump(n)
+
+		// split indexed/var name from end
+		p2 = p2[:xx+1]
+		// if x1 > x2 {
+		// 	p2 = p2[:x1+1]
+		// } else {
+		// 	p2 = p2[:x2+1]
+		// }
 
 		// split array from tuple
 		x1 = strings.LastIndex(p2, "]")
@@ -148,14 +169,27 @@ func parseEventArgs(eventArgs string) (eventSelectorTree, error) {
 		p := strings.Split(p1, ",")
 		s := ""
 		p1indexed := []bool{}
+		p1names := []string{}
+
 		for _, a := range p {
 			arg := strings.Split(strings.TrimSpace(a), " ")
-			// TODO: lets get the arg name too
+
+			if len(arg) > 3 {
+				return out, fmt.Errorf("invalid event argument format")
+			}
 
 			if len(arg) == 3 && arg[1] == "indexed" {
 				p1indexed = append(p1indexed, true)
+				p1names = append(p1names, arg[2])
+			} else if len(arg) == 3 && arg[1] != "indexed" {
+				return out, fmt.Errorf("invalid event indexed argument format")
 			} else if len(arg) > 0 && arg[0] != "" {
 				p1indexed = append(p1indexed, false)
+				if len(arg) > 1 {
+					p1names = append(p1names, arg[1])
+				} else {
+					p1names = append(p1names, "")
+				}
 			}
 
 			typ := strings.TrimSpace(arg[0])
@@ -168,22 +202,24 @@ func parseEventArgs(eventArgs string) (eventSelectorTree, error) {
 		}
 		out.left = s
 		out.indexed = p1indexed
+		out.names = p1names
 	}
 
 	// p2
 	if len(p2) > 0 {
-		out2, err := parseEventArgs(p2)
+		out2, err := parseEventArgs(p2, iteration+1)
 		if err != nil {
 			return out, err
 		}
 		out.tuple = append(out.tuple, out2)
 		out.tupleArray = p2ar
 		out.indexed = append(out.indexed, p2indexed)
+		out.names = append(out.names, p2name)
 	}
 
 	// p3
 	if len(p3) > 0 {
-		out3, err := parseEventArgs(p3)
+		out3, err := parseEventArgs(p3, iteration+1)
 		if err != nil {
 			return out, err
 		}
@@ -193,10 +229,11 @@ func parseEventArgs(eventArgs string) (eventSelectorTree, error) {
 	return out, nil
 }
 
-func groupEventSelectorTree(t eventSelectorTree, include bool) (string, []string, []bool, error) {
+func groupEventSelectorTree(t eventSelectorTree, include bool) (string, []string, []bool, []string, error) {
 	out := ""
 	typs := []string{}
 	indexed := []bool{}
+	names := []string{}
 
 	a := ""
 	b := ""
@@ -214,12 +251,13 @@ func groupEventSelectorTree(t eventSelectorTree, include bool) (string, []string
 			}
 		}
 		indexed = append(indexed, t.indexed...)
+		names = append(names, t.names...)
 	}
 
 	for _, child := range t.tuple {
-		s, _, _, err := groupEventSelectorTree(child, false)
+		s, _, _, _, err := groupEventSelectorTree(child, false)
 		if err != nil {
-			return "", nil, nil, err
+			return "", nil, nil, nil, err
 		}
 		if s != "" {
 			b = "(" + strings.TrimRight(s, ",") + ")"
@@ -231,9 +269,9 @@ func groupEventSelectorTree(t eventSelectorTree, include bool) (string, []string
 	}
 
 	for _, child := range t.right {
-		s, rtyps, i, err := groupEventSelectorTree(child, true)
+		s, rtyps, rindexed, rnames, err := groupEventSelectorTree(child, true)
 		if err != nil {
-			return "", nil, nil, err
+			return "", nil, nil, nil, err
 		}
 		if s != "" {
 			c = strings.TrimRight(s, ",") + ","
@@ -243,7 +281,8 @@ func groupEventSelectorTree(t eventSelectorTree, include bool) (string, []string
 				typs = append(typs, v)
 			}
 		}
-		indexed = append(indexed, i...)
+		indexed = append(indexed, rindexed...)
+		names = append(names, rnames...)
 	}
 
 	if len(a) > 0 {
@@ -256,7 +295,7 @@ func groupEventSelectorTree(t eventSelectorTree, include bool) (string, []string
 		out += c
 	}
 
-	return strings.TrimRight(out, ","), typs, indexed, nil
+	return strings.TrimRight(out, ","), typs, indexed, names, nil
 }
 
 func findParensCloseIndex(args string) (int, error) {
--- ethcoder/events_parser_test.go
+++ ethcoder/events_parser_test.go
@@ -3,94 +3,90 @@ package ethcoder
 import (
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/require"
 )
 
 func TestParseEventArgs(t *testing.T) {
 	cases := []struct {
 		in         string
-		expect     string
-		numArgs    int
-		numIndexed int
+		sig        string
+		argIndexed []bool
+		argNames   []string
 	}{
 		{
-			"bytes32 num,   address[] indexed from, uint256 num,   (   address op,   (uint256 val, bytes32 data)) indexed yes,   address,  (int128 a, int64 b), uint256",
+			"bytes32 num,   address[] indexed from, uint256 val,   (   address op,   (uint256 val, bytes32 data)) indexed yes,   address,  (int128 a, int64 b), uint256",
 			"bytes32,address[],uint256,(address,(uint256,bytes32)),address,(int128,int64),uint256",
-			7,
-			2,
+			[]bool{false, true, false, true, false, false, false},
+			[]string{"num", "from", "val", "yes", "", "", ""},
 		},
 		{ // its not actually valid selector, but use it for parser testing
-			"bytes indexed blah, uint256[2][] yes, ( address yo, uint256[2] yes )[2][] indexed okay, address yes",
+			"bytes indexed blah, uint256[2][] yes, ( address yo, uint256[2] no )[2][] indexed okay, address last",
 			"bytes,uint256[2][],(address,uint256[2])[2][],address",
-			4,
-			2,
+			[]bool{true, false, true, false},
+			[]string{"blah", "yes", "okay", "last"},
 		},
 		{ // its not actually valid selector, but use it for parser testing
 			"address from, (  uint256 num, address cool, (  address op, uint256 val )[2] hmm)[][] lol, uint256 val",
 			"address,(uint256,address,(address,uint256)[2])[][],uint256",
-			3,
-			0,
+			[]bool{false, false, false},
+			[]string{"from", "lol", "val"},
 		},
 		{
 			"address indexed from, address indexed to, uint256 value",
 			"address,address,uint256",
-			3,
-			2,
+			[]bool{true, true, false},
+			[]string{"from", "to", "value"},
 		},
 		{
 			"bytes32,address,address indexed yes,((uint32,uint32,uint32,address,address,bool,bytes,uint256,address,uint256,uint256,uint256,bytes32),address[],bytes[],address,bytes) indexed cool,address,uint256,address indexed last",
 			"bytes32,address,address,((uint32,uint32,uint32,address,address,bool,bytes,uint256,address,uint256,uint256,uint256,bytes32),address[],bytes[],address,bytes),address,uint256,address",
-			7,
-			3,
+			[]bool{false, false, true, true, false, false, true},
+			[]string{"", "", "yes", "cool", "", "", "last"},
 		},
-		//..
 		{
 			"(address,uint256,string,string) indexed okay",
 			"(address,uint256,string,string)",
-			1,
-			1,
+			[]bool{true},
+			[]string{"okay"},
 		},
 		{
 			"bytes32,address,address,address,(uint8,address,uint256,uint256)[] indexed good,(uint8,address,uint256,uint256,address)[] indexed last",
 			"bytes32,address,address,address,(uint8,address,uint256,uint256)[],(uint8,address,uint256,uint256,address)[]",
-			6,
-			2,
+			[]bool{false, false, false, false, true, true},
+			[]string{"", "", "", "", "good", "last"},
 		},
 		{
 			"address,(uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,address,uint40,uint40)",
 			"address,(uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,address,uint40,uint40)",
-			2,
-			0,
+			[]bool{false, false},
+			[]string{"", ""},
 		},
 		{
 			"bytes32,address,address,address,(uint8,address,uint256,uint256)[],(uint8,address,uint256,uint256,address)[]",
 			"bytes32,address,address,address,(uint8,address,uint256,uint256)[],(uint8,address,uint256,uint256,address)[]",
-			6,
-			0,
+			[]bool{false, false, false, false, false, false},
+			[]string{"", "", "", "", "", ""},
 		},
 	}
 
 	for _, c := range cases {
-		tree, err := parseEventArgs(c.in)
+		tree, err := parseEventArgs(c.in, 0)
 		require.NoError(t, err)
-		// spew.Dump(tree)
+		spew.Dump(tree)
 
-		out, typs, indexed, err := groupEventSelectorTree(tree, true)
+		out, typs, indexed, names, err := groupEventSelectorTree(tree, true)
 		require.NoError(t, err)
-		require.Equal(t, c.expect, out)
-		// spew.Dump(typs)
-		// spew.Dump(indexed)
+		require.Equal(t, c.sig, out)
+		spew.Dump(typs)
+		spew.Dump(indexed)
+		spew.Dump(names)
 
-		require.Equal(t, c.numArgs, len(typs))
-		require.Equal(t, c.numArgs, len(indexed))
-
-		numIndexed := 0
-		for _, i := range indexed {
-			if i {
-				numIndexed++
-			}
-		}
-		require.Equal(t, c.numIndexed, numIndexed)
+		require.Equal(t, len(c.argNames), len(typs))
+		require.Equal(t, len(c.argNames), len(indexed))
+		require.Equal(t, len(c.argNames), len(names))
+		require.Equal(t, c.argIndexed, indexed)
+		require.Equal(t, c.argNames, names)
 
 		// ok, err := ValidateEventSig(fmt.Sprintf("Test(%s)", out))
 		// require.NoError(t, err)
