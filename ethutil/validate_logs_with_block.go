67c021d08c287119c8e0b2ee58145a20fbcef6a0
Taylan Pince <taylanpince@gmail.com>
2025-12-05 18:48:43 +0100

    Remove func signature from validate

--- ethutil/validate_logs_with_block.go
+++ ethutil/validate_logs_with_block.go
@@ -16,7 +16,7 @@ type LogsFilterFunc func(logs []types.Log, header *types.Header, block *types.Bl
 
 // ValidateLogsWithBlockHeader validates that the logs come from the given block
 // by comparing the calculated bloom against the header bloom.
-var ValidateLogsWithBlockHeader LogsBloomCheckFunc = func(logs []types.Log, header *types.Header) bool {
+func ValidateLogsWithBlockHeader(logs []types.Log, header *types.Header) bool {
 	return bytes.Equal(ConvertLogsToBloom(logs).Bytes(), header.Bloom.Bytes())
 }
 
